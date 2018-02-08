b29724ecce7739f38b226e55f2fe460649fe1397
Pete <pbianchi192@gmail.com>
2018-02-08 16:30:21 -0600

    First commit

---
+++ multiples_sum_average.py
@@ -0,0 +1,13 @@
+for i in range (1, 1000):
+    if i % 2 != 0:
+        print i
+
+for j in range (5, 1000000):
+    if j % 5 == 0:
+        print j
+
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+
+b = [1, 2, 5, 10, 255, 3]
+print sum(b)/len(b)
